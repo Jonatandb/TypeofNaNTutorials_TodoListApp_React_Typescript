e661f61495aeefc4edeed21ac0540ea9230745f1
Jonatandb <jonatandb@gmail.com>
2019-12-31 20:04:48 -0300

    Creating a Todo List App in React using Typescript (Part 2)

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,33 @@
 // Creating a Todo List App in React using Typescript (Part 1)
 //    https://www.youtube.com/watch?v=ODvirqIC09A
 
-import React from 'react';
-import { ToDoListItem } from './ToDoListItem';
+import React, { useState } from 'react';
+import { ToDoList } from './ToDoList';
 
-const todos: Array<Todo> = [
+const initialTodos: Array<Todo> = [
   { text: "Estudiar TypeScript", complete: true },
   { text: "Dormir", complete: false },
 ];
 
 const App: React.FC = () => {
+  const [todos, setTodos] = useState(initialTodos);
+
+  const toggleTodo = (selectedTodo: Todo) => {
+    const newTodos = todos.map(todo => {
+      if (todo === selectedTodo) {
+        return {
+          ...todo,
+          complete: !todo.complete
+        };
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+  };
+
   return (
     <>
-      <ToDoListItem todo={todos[0]} />
-      <ToDoListItem todo={todos[1]} />
+      <ToDoList todos={todos} toggleTodo={toggleTodo} />
     </>
   );
 }
--- src/ToDoListItem.tsx
+++ src/ToDoListItem.tsx
@@ -2,13 +2,18 @@ import React from 'react';
 import "./ToDoListItem.css";
 
 interface ToDoListItemProps {
-    todo: Todo
+    todo: Todo,
+    toggleTodo: ToggleTodo
 }
 
-export const ToDoListItem: React.FC<ToDoListItemProps> = ({ todo }) => {
+export const ToDoListItem: React.FC<ToDoListItemProps> = ({ todo, toggleTodo }) => {
     return <li>
         <label className={todo.complete ? "complete" : undefined}>
-            <input type="checkbox" checked={todo.complete} />
+            <input
+                type="checkbox"
+                checked={todo.complete}
+                onChange={() => toggleTodo(todo)}
+            />
             {todo.text}
         </label>
     </li >;
